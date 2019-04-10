23002509815b9936bd1a1d2cc6811ea7a064bc0b
LHB <804433577@qq.com>
2019-04-10 17:59:15 +0800

    #update composer.json!

--- lockDoor/src/request/LockDoorRequest.php
+++ lockDoor/src/request/LockDoorRequest.php
@@ -40,38 +40,40 @@ trait LockDoorRequest
         } catch (\Exception $exception) {
             return $exception->getMessage();
         }
-
-//        $curl = curl_init();
-//
-//        curl_setopt_array($curl, array(
-//            CURLOPT_URL => "https://www.fengchaoiot.com/api/accessToken",
-//            CURLOPT_RETURNTRANSFER => true,
-//            CURLOPT_ENCODING => "",
-//            CURLOPT_MAXREDIRS => 10,
-//            CURLOPT_TIMEOUT => 30,
-//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//            CURLOPT_CUSTOMREQUEST => "POST",
-//            CURLOPT_POSTFIELDS => json_encode($params),
-//            CURLOPT_HTTPHEADER => array(
-//                "cache-control: no-cache",
-//                "content-type: application/json",
-//                "postman-token: ebc70695-f35c-ee66-98ee-6f096101ab6f",
-//                "x-accept-version: beehive.v1"
-//            ),
-//        ));
-//
-//        $response = curl_exec($curl);
-//        $err = curl_error($curl);
-//
-//        curl_close($curl);
-//
-//        if ($err) {
-//            echo "cURL Error #:" . $err;
-//        } else {
-//            echo $response;
-//            echo "\n";
-//        }
+    }
 
 
+    public function requestByCURL($params)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://www.fengchaoiot.com/api/accessToken",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($params),
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "content-type: application/json",
+                "postman-token: ebc70695-f35c-ee66-98ee-6f096101ab6f",
+                "x-accept-version: beehive.v1"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+            echo "\n";
+        }
     }
 }
