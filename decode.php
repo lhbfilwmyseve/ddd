b1a11af94e3d4002aa1e795ac9ff83d8cab707f6
LHB <804433577@qq.com>
2019-06-14 11:49:46 +0800

    update !!!

---
+++ decode.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by LHB
+ * User: LHB
+ * Date: 2019/5/16
+ * Time: 9:42
+ * Email:498807233@qq.com
+ */
+
+$str = 'BgYaw8IoLIfzc45ZxgAGBgYGBgY=';
+echo '$str  '.$str;
+echo PHP_EOL;
+echo 'base64_decode '.base64_decode($str);
+echo PHP_EOL;
